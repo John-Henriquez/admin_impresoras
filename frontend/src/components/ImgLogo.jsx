83b75508307b2ccd9b71ef913836d268b436cca4
cristóbal <criskywww@gmail.com>
2024-07-29 21:44:58 -0400

    MOD: Información General

--- frontend/src/components/ImgLogo.jsx
+++ frontend/src/components/ImgLogo.jsx
@@ -2,9 +2,9 @@
 const ImgLogo = () => {
 	return (
 		<aside className="img_logo">
-			<img src="cohete.png" alt="Logo cohete Metodología de desarrollo" />
-			<h1>Ayudantía</h1>
-			<h2>Metodologia de desarrollo</h2>
+			<img src="ubb-white.png" alt="Logo UBB" />
+			<h1>Universidad del Bío-Bío</h1>
+			<h2>Centro de fotocopiado</h2>
 		</aside>
 	);
 };
